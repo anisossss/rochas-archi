593bce511a9915bc4814c3db323ed7a6dc8e5445
anis <anis.khalef98@gmail.com>
2023-10-28 06:23:21 +0100

    latest

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "/index.html"));
 });
 app.get("/works", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "works.html"));
+  res.sendFile(path.join(__dirname, "public", "/works.html"));
 });
 app.get("/fluid", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "fluid.html"));
+  res.sendFile(path.join(__dirname, "public", "/fluid.html"));
 });
 
 app.get("/scroll", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "scroll.html"));
+  res.sendFile(path.join(__dirname, "public", "/scroll.html"));
 });
 
 app.listen(port, () => {
